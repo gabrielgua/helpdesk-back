8be4815e0cda8fba9f7858672708aa13777e97ec
gabrielgua <gabrieljmym13@gmail.com>
2022-05-13 17:11:12 -0300

    find tecnico by id implementado

--- src/main/java/com/gabriel/helpdesk/controller/TecnicoController.java
+++ src/main/java/com/gabriel/helpdesk/controller/TecnicoController.java
@@ -1,2 +1,25 @@
-package com.gabriel.helpdesk.controller;public class TecnicoController {
+package com.gabriel.helpdesk.controller;
+
+import com.gabriel.helpdesk.domain.Tecnico;
+import com.gabriel.helpdesk.services.TecnicoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("tecnicos")
+public class TecnicoController {
+
+    @Autowired
+    private TecnicoService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Tecnico> buscarTecnicoPorId(@PathVariable("id") Long id) {
+        Tecnico tecnico = service.buscarPorId(id);
+        return ResponseEntity.ok().body(tecnico);
+    }
 }
+
--- src/main/java/com/gabriel/helpdesk/services/TecnicoService.java
+++ src/main/java/com/gabriel/helpdesk/services/TecnicoService.java
@@ -1,2 +1,21 @@
-package com.gabriel.helpdesk.services;public class TecnicoService {
+package com.gabriel.helpdesk.services;
+
+import com.gabriel.helpdesk.domain.Tecnico;
+import com.gabriel.helpdesk.repositories.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class TecnicoService {
+
+    @Autowired
+    private TecnicoRepository repository;
+
+    @Transactional(readOnly = true)
+    public Tecnico buscarPorId(Long id) {
+        return repository.findById(id).orElse(null);
+    }
 }
